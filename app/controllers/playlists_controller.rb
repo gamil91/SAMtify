6fbdef12d8b9042faad0be4630de2d1bddccaead
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-08 09:52:20 -0800

    Song, Artist, and PlaylistSong

--- app/controllers/playlists_controller.rb
+++
@@ -1,7 +0,0 @@
-class PlaylistsController < ApplicationController
-
-    def show
-        @playlist = Playlist.find(params[:id])
-        @songs = @playlist.playlist_songs.map {|ps| ps.song }
-    end
-end
