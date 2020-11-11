7025898c8423e3439c2005fe5aabd32ee21914df
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-11 10:20:51 -0800

    formatted login, users show, playlist show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,8 +3,6 @@ class ArtistsController < ApplicationController
     def index
         if params[:query]
             @artists = Artist.find_by_query(params[:query]) 
-        elsif params[:sort]
-            @artists = Artist.all.sort_by {|artist| artist.name}
         else
             @artists = Artist.all
         end
