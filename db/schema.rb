f8a57dd744b88285249b81f6700086a871e068c3
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-11 12:09:43 -0800

    subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_051415) do
+ActiveRecord::Schema.define(version: 2020_11_11_192604) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2020_11_11_051415) do
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_subscriptions_on_artist_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -69,4 +78,6 @@ ActiveRecord::Schema.define(version: 2020_11_11_051415) do
   add_foreign_key "playlist_songs", "songs"
   add_foreign_key "playlists", "users"
   add_foreign_key "songs", "artists"
+  add_foreign_key "subscriptions", "artists"
+  add_foreign_key "subscriptions", "users"
 end
